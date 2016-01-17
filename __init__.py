cc621942f2ad9807898833b91b4188e40b1203f1
Oscar Melin <oscarme@kth.se>
2016-01-17 23:39:29 +0100

    as d

--- __init__.py
+++ __init__.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def homepage():
     return render_template("main.html")
 
+@app.route("/dashboard/")
+def dashboard():
+    return render_template("dashboard.html")
+
 if __name__ == "__main__":
     app.run()
