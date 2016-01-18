9d0cd0d2bc9c94ba7fb6079f9f559cdd4ddadb6b
Oscar Melin <oscarme@kth.se>
2016-01-18 21:54:37 +0100

    login stuff

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 app = Flask(__name__)
 
 @app.route("/")
@@ -9,9 +9,21 @@ def homepage():
 def dashboard():
     return render_template("dashboard.html")
 
+@app.route("/login/", methods = ["GET", "POST"])
+def login_page():
+    return render_template("login.html")
+
 @app.errorhandler(404)
 def page_not_found(e):
     return("four oh four")
+
+@app.errorhandler(405)
+def page_not_found(e):
+    return("Request not permitted")
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return("five hundred")
 
 if __name__ == "__main__":
     app.run()
